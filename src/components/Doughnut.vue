138e053d803bb527465c7b3d23e14eb199490464
RomainGuarinoni <romain.guar91@gmail.com>
2021-05-28 15:01:52 +0200

    fix doughnut update

--- src/components/Doughnut.vue
+++ src/components/Doughnut.vue
@@ -5,63 +5,69 @@
 </template>
 
 <script>
-import Chart from "chart.js";
-export default {
-  props: ["dataRythme", "labelRythme"],
-  data() {
-    return {
-      graph: undefined,
-    };
-  },
-  watch: {
-    /* eslint-disable */
-    dataRythme: function(val) {
-      this.graph.data.datasets[0].data = [];
-      this.graph.data.datasets[0].data = val;
-      this.graph.update();
+  import Chart from "chart.js";
+  export default {
+    props: ["echecProps", "labelRythme", "reussiteProps"],
+    data() {
+      return {
+        graph: undefined,
+      };
     },
-    /* eslint-disable */
-  },
-  mounted: function() {
-    console.log(this.labelRythme);
-    console.log(typeof this.dataRythme);
-    let ctx = document.getElementById("canvasRythme");
-    this.graph = new Chart(ctx, {
-      type: "pie",
-      data: {
-        labels: this.labelRythme,
-        datasets: [
-          {
-            data: this.dataRythme,
-            backgroundColor: ["#1abb77", "#fbae1b"],
-            hoverOffset: 4,
-            borderColor: "transparent",
-          },
-        ],
+    watch: {
+      /* eslint-disable */
+      echecProps: function(val) {
+        console.log("hello");
+        this.graph.data.datasets[0].data = [];
+        this.graph.data.datasets[0].data = [this.reussiteProps, val];
+        this.graph.update();
       },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            labels: {
-              color: "white", //set your desired color
+      reussiteProps: function(val) {
+        his.graph.data.datasets[0].data = [];
+        this.graph.data.datasets[0].data = [val, vthis.echecProps];
+        this.graph.update();
+      },
+      /* eslint-disable */
+    },
+    mounted: function() {
+      console.log(this.reussiteProps);
+      console.log(this.echecProps);
+      let ctx = document.getElementById("canvasRythme");
+      this.graph = new Chart(ctx, {
+        type: "pie",
+        data: {
+          labels: this.labelRythme,
+          datasets: [
+            {
+              data: [this.reussiteProps, this.echecProps],
+              backgroundColor: ["#1abb77", "#fbae1b"],
+              hoverOffset: 4,
+              borderColor: "transparent",
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          plugins: {
+            legend: {
+              labels: {
+                color: "white", //set your desired color
+              },
             },
           },
+          interaction: {
+            mode: "index",
+            intersect: false,
+          },
         },
-        interaction: {
-          mode: "index",
-          intersect: false,
-        },
-      },
-    });
-  },
-};
+      });
+    },
+  };
 </script>
 
 <style scoped>
-.containerGraph {
-  flex: 1;
-  height: 100%;
-}
+  .containerGraph {
+    flex: 1;
+    height: 100%;
+  }
 </style>
--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -5,106 +5,106 @@
 </template>
 
 <script>
-import Chart from "chart.js";
-export default {
-  props: ["dataNote", "labelNote"],
-  data() {
-    return {
-      graph: undefined,
-    };
-  },
-  watch: {
-    /* eslint-disable */
-    dataNote: function(val) {
-      this.graph.data.datasets[0].data = [];
-      this.graph.data.datasets[0].data = val;
-      this.graph.update();
+  import Chart from "chart.js";
+  export default {
+    props: ["dataNote", "labelNote"],
+    data() {
+      return {
+        graph: undefined,
+      };
     },
-    /* eslint-disable */
-    labelNote: function(val) {
-      this.graph.data.labels = [];
-      this.graph.data.labels = val;
-      this.graph.update();
+    watch: {
+      /* eslint-disable */
+      dataNote: function(val) {
+        this.graph.data.datasets[0].data = [];
+        this.graph.data.datasets[0].data = val;
+        this.graph.update();
+      },
+      /* eslint-disable */
+      labelNote: function(val) {
+        this.graph.data.labels = [];
+        this.graph.data.labels = val;
+        this.graph.update();
+      },
     },
-  },
-  mounted: function() {
-    let ctx = document.getElementById("canvas");
-    this.graph = new Chart(this.$refs.ref, {
-      type: "line",
-      data: {
-        labels: this.labelNote,
-        datasets: [
-          {
-            label: "Note en %",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "#ff6384",
-            borderColor: "#ff6384",
-            borderCapStyle: "butt",
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: "miter",
-            pointBorderColor: "#ff6384",
-            pointBackgroundColor: "#ff6384",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "#ff6384",
-            pointHoverBorderColor: "#ff6384",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: this.dataNote,
+    mounted: function() {
+      let ctx = document.getElementById("canvas");
+      this.graph = new Chart(this.$refs.ref, {
+        type: "line",
+        data: {
+          labels: this.labelNote,
+          datasets: [
+            {
+              label: "Note en %",
+              fill: false,
+              lineTension: 0.1,
+              backgroundColor: "#ff6384",
+              borderColor: "#ff6384",
+              borderCapStyle: "butt",
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: "miter",
+              pointBorderColor: "#ff6384",
+              pointBackgroundColor: "#ff6384",
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: "#ff6384",
+              pointHoverBorderColor: "#ff6384",
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: this.dataNote,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            yAxis: {
+              ticks: {
+                backdropColor: "rgba(255, 255, 255, 1)",
+                color: "rgba(255, 255, 255, 0.5)",
+                padding: 0,
+                beginAtZero: true,
+              },
+              grid: {
+                color: "rgba(255,255,255,0.2)",
+                display: false,
+              },
+              max: 100,
+            },
+            xAxis: {
+              ticks: {
+                backdropColor: "rgba(255, 255, 255, 1)",
+                color: "rgba(255, 255, 255, 0.5)",
+              },
+              grid: {
+                color: "rgba(255,255,255,0.2)",
+                display: false,
+              },
+            },
           },
-        ],
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          yAxis: {
-            ticks: {
-              backdropColor: "rgba(255, 255, 255, 1)",
-              color: "rgba(255, 255, 255, 0.5)",
-              padding: 0,
-              beginAtZero: true,
+          plugins: {
+            legend: {
+              labels: {
+                color: "white", //set your desired color
+              },
             },
-            grid: {
-              color: "rgba(255,255,255,0.2)",
-              display: false,
-            },
-            max: 100,
           },
-          xAxis: {
-            ticks: {
-              backdropColor: "rgba(255, 255, 255, 1)",
-              color: "rgba(255, 255, 255, 0.5)",
-            },
-            grid: {
-              color: "rgba(255,255,255,0.2)",
-              display: false,
-            },
+          interaction: {
+            mode: "index",
+            intersect: false,
           },
         },
-        plugins: {
-          legend: {
-            labels: {
-              color: "white", //set your desired color
-            },
-          },
-        },
-        interaction: {
-          mode: "index",
-          intersect: false,
-        },
-      },
-    });
-  },
-};
+      });
+    },
+  };
 </script>
 
 <style scoped>
-.containerGraph {
-  flex: 1;
-  height: 100%;
-}
+  .containerGraph {
+    flex: 1;
+    height: 100%;
+  }
 </style>
